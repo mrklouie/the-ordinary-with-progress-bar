6076cdbc86cf0b56589e62c38453b805a2dfd9ef
mrklouie <mark.17dullavin@gmail.com>
2023-01-31 10:05:16 +0800

    Jan-31-2023
    Added progressbar

---
+++ index.a7a7847f.js
@@ -0,0 +1,2 @@
+const container=document.querySelector(".container"),body=document.querySelector("body");paceOptions={ajax:!0,document:!0,eventLag:!1},Pace.on("done",(function(){container.classList.add("active"),body.classList.add("active")}));
+//# sourceMappingURL=index.a7a7847f.js.map
---
+++ skincares.1d71ddd2.js
@@ -0,0 +1,2 @@
+const navbar=document.querySelector(".navbar"),navbarHeight=navbar.getBoundingClientRect().height,heroWrapper=document.querySelector("#skincare-page .hero-wrapper"),skincareHero=document.querySelector("#skincare-hero-wrapper");heroWrapper.style.top=`${navbarHeight}px`,heroWrapper.style.setProperty("--hero-height",`${navbarHeight}px`);
+//# sourceMappingURL=skincares.1d71ddd2.js.map
